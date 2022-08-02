c3fafbb99bfbf2c129c2b034c9badef09c7dc0dc
Volodymyr <coremio3@gmail.com>
2022-08-02 22:56:45 +0200

    delete yard game

--- js/game.js
+++ js/game.js
@@ -1,51 +1,40 @@
+// document.addEventListener("DOMContentLoaded", function () {
 const cont = document.getElementById("container");
 const form = document.querySelector("form");
-// let but = document.createElement("button");
 const btnSize = document.getElementById("size");
 let row = document.getElementById("row");
 let column = document.getElementById("column");
+let div1 = document.createElement("div");
+div1.id = "sizeDiv";
 
 btnSize.addEventListener("click", (e) => {
-  let divDelet = document.getElementById("sizeDiv");
+  let btnDelet = document.querySelectorAll(".btnDelete");
 
-  if (divDelet !== null) {
-    cont.removeChild(divDelet);
+  if (btnDelet.length > 0) {
+    for (const elem of btnDelet) {
+      elem.remove();
+    }
   }
 
-  let div = document.createElement("div");
-  div.id = "sizeDiv";
   for (let i = 0; i < row.value; i++) {
     for (let j = 0; j < column.value; j++) {
       let but = document.createElement("button");
       but.id = `${i}-${j}`;
       but.classList.add("btnSize");
-      div.appendChild(but);
+      but.classList.add("btnDelete");
+      div1.appendChild(but);
     }
 
-    div.appendChild(document.createElement("p"));
+    div1.appendChild(document.createElement("p"));
+    cont.appendChild(div1);
   }
-  cont.appendChild(div);
 });
-let divYard = document.getElementById("sizeDiv");
-// document.addEventListener('DOMContentLoaded',function() {
-// let divYard = document.getElementById("sizeDiv");
-//   divYard?.addEventListener("click", (e) => {
-//   console.log(e)
-// })
-// })
 
-divYard?.addEventListener('click', (e) => {
-  if (e.target && e.target.id == '0-1') {
-    console.log("dddddddddddddddd");
+div1?.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.localName === "button") {
+    let i = e.target.attributes.id.nodeValue;
+    let chanDiv = document.getElementById(i);
+    chanDiv.classList.add("red");
   }
-})
-function swapper() {
-  toggleClass(document.getElementById("overlay"), "open");
-}
-
-if (divYard) {
-  divYard.click( (e) => {
-    console.log(e);
-  });
-}
-// let divYard = document.getElementById("sizeDiv");
+});
